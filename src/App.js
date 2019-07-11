93ab3817d1b296bc3c8b7e5aed38436702ce227e
Andreea Hanson <andreea.hanson7@gmail.com>
2019-07-11 10:12:43 -0600

    Add reservation form component

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import React, { Component } from 'react';
 import './App.css';
 import getData from './apiCalls';
 import fetchData from './apiCalls';
-import ReservationContainer from './ReservationContainer'
+import ReservationContainer from './ReservationContainer';
+import Form from './Form';
 
 class App extends Component {
   constructor() {
@@ -19,13 +20,17 @@ class App extends Component {
       .catch(error => this.setState({error: 'Error fetching data'}))
   }
 
+  addReservation = (newReservation) => {
+    this.setState({...this.state.reservations, newReservation})
+  }
+
   render() {
     let reservations = this.state.reservations
     return (
       <div className="App">
         <h1 className='app-title'>Turing Cafe Reservations</h1>
         <div className='resy-form'>
-
+          <Form reservations={reservations} addReservation={this.addReservation}/>
         </div>
         <div className='resy-container'>
           <ReservationContainer reservations={reservations}/>
